55ab3d8d82bf4b2b5e84677485d0a72e6c305fcc
Charles Bernasconi <charles.bernasconi@xing.com>
2019-08-07 17:01:31 +0200

    non-cluster-deployment

--- script/build.sh
+++ script/build.sh
@@ -4,5 +4,5 @@ docker build \
   --build-arg APP_NAME=k8s_example \
   --build-arg APP_VSN=0.1.0 \
   --build-arg SKIP_PHOENIX=true \
-  -t k8s_example:latest \
+  -t chazsconi/k8s-example:latest \
   .
